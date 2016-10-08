ac7c099805fdcdbf6e55a62df852aaaea4330a15
brianaraujo84@gmail.com <abaraujo@asfi.gov.bo>
2016-10-08 10:11:43 -0400

    Commit inicial

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+    this is a simple Java program
+    Call this file HelloWorld.java
+*/
+
+class HelloWorld{
+    // A java program begins with a call to main()
+public static void main(String args[]){
+        System.out.println("Hello from Java!!");
+    }
+}
